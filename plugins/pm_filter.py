d6148c066208b9d7930d9d1939c7b992f0363d5e
Abridrosariyo <97105953+Abridrosariyo@users.noreply.github.com>
2023-04-05 10:53:09 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1633,7 +1633,6 @@ async def auto_filter(client, msg, spoll=False):
         btn.append(
             [InlineKeyboardButton(text=f"📜 ᴘᴀɢᴇ 1/{math.ceil(int(total_results) / 6)}", callback_data="pages"),
              InlineKeyboardButton(text="ɴᴇxᴛ▷", callback_data=f"next_{req}_{key}_{offset}")]
-
         )
     else:
         btn.append(
