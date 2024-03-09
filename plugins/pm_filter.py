52811ce3d9c67df27e59e3245a40d99c0228ba61
SUNEESH-A10 <161802981+SUNEESH-A10@users.noreply.github.com>
2024-03-09 12:37:22 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1323,13 +1323,9 @@ async def auto_filter(client, msg, spoll=False):
                     if imdb and imdb.get('poster'):
                         await message.reply_photo(photo=imdb.get('poster'), caption=LuciferMoringstar.GET_MOVIE_7.format(mention=message.from_user.mention, query=search, title=imdb.get('title'), genres=imdb.get('genres'), year=imdb.get('year'), rating=imdb.get('rating'), short=imdb.get('short_info'), url=imdb['url']), reply_markup=reply_markup) 
                         return
-                    else:
-                        return
                 else:
                     return
-        else:
-            return
-    else:
+            else:
         settings = await get_settings(msg.message.chat.id)
         message = msg.message.reply_to_message  # msg will be callback query
         search, files, offset, total_results = spoll
