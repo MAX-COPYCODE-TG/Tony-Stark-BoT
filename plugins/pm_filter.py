b6843cb5ee11f13ddd6537a26feff8dce32fd47f
SUNEESH-A10 <161802981+SUNEESH-A10@users.noreply.github.com>
2024-03-10 10:56:25 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1305,7 +1305,6 @@ async def cb_handler(client: Client, query: CallbackQuery):
 
 async def auto_filter(client, msg, spoll=False):
     if not spoll:
-        meif not spoll:
         message = msg
         settings = await get_settings(message.chat.id)
         if message.text.startswith("/"): return  # ignore commands
