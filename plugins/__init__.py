741960b0442c97947b6911d4839636b1c0eaabf7
Soul pc <119080077+soulpc@users.noreply.github.com>
2024-09-22 21:06:14 +0530

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -4,8 +4,8 @@ routes = web.RouteTableDef()
 
 @routes.get("/", allow_head=True)
 async def root_route_handler(request):
-    return web.json_response("Mᴋɴ Bᴏᴛᴢ")
-
+    return web.json_response("MATRIX")
+  
 async def web_server():
     web_app = web.Application(client_max_size=30000000)
     web_app.add_routes(routes)
